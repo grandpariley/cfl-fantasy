35319ac5fd8db9aa98c406c2cd6afb2bc7371390
grandpariley <rileyherman@duck.com>
2024-05-31 15:56:26 -0600

    better illustration of what this looks like

--- pkg/model/all_different_teams_less_than_10000.py
+++ pkg/model/all_different_teams_less_than_10000.py
@@ -5,11 +5,13 @@ picked_teams = []
 
 class AllDifferentTeamsAndLessThan10000(Model):
     def score(self, d):
-        if d['team'] in picked_teams or int(d['price']) > 10000:
+        if int(d['price']) > 10000:
             return 0
         return float(d['projected_points'])
 
     def re_score(self, d, picks, last_picked, budget):
         if last_picked:
             picked_teams.append(last_picked['team'])
-        return self.score(d)
+        if d['team'] in picked_teams or int(d['price']) > 10000:
+            return 0
+        return float(d['projected_points'])
